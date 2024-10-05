10bc2ac81cfc671d8442e5b541a209a5e5033d1c
45degree <1098902652@qq.com>
2024-10-05 17:14:52 +0800

    add some target function

--- library/target_instance.lua
+++ library/target_instance.lua
@@ -131,13 +131,72 @@ function Target:objectfile(source) end
 ---
 --- [Open in browser](https://xmake.io/#/manual/target_instance?id=targetsourcebatches)
 ---
---- @return { [string]: SourceBatch }
+--- It can get all the source files added by `add_files` and store them separately according to different source file types.
+---
+--- The approximate structure is as follows:
+--- ```lua
+--- {
+---   "c++.build" = {
+---     objectfiles = {
+---       "build/.objs/test/macosx/x86_64/release/src/main.cpp.o"
+---     },
+---     rulename = "c++.build",
+---     sourcekind = "cxx",
+---     sourcefiles = {
+---       "src/main.cpp"
+---     },
+---     dependfiles = {
+---       "build/.deps/test/macosx/x86_64/release/src/main.cpp.o.d"
+---     }
+---   },
+---   "asm.build" = {
+---     objectfiles = {
+---       "build/.objs/test/macosx/x86_64/release/src/test.S.o"
+---     },
+---     rulename = "asm.build",
+---     sourcekind = "as",
+---     sourcefiles = {
+---       "src/test.S"
+---     },
+---     dependfiles = {
+---       "build/.deps/test/macosx/x86_64/release/src/test.S.o.d"
+---     }
+---   }
+--- }
+--- ```
+---
+--- @example
+--- We can traverse to obtain and process each type of source file.
+---
+--- ```lua
+--- for _, sourcebatch in pairs(target:sourcebatches()) do
+---     local sourcekind = sourcebatch.sourcekind
+---     if sourcekind == "cc" or sourcekind == "cxx" or sourcekind == "as" then
+---         for _, sourcefile in ipairs(sourcebatch.sourcefiles) do
+---             - TODO
+---         end
+---     end
+--- end
+--- ```
+---
+--- Where sourcekind is the type of each source file, cc is the c file type, cxx is the c++ source file, and as is the asm source file.
+--- sourcebatch corresponds to each type of source file batch, corresponding to a batch of source files of the same type.
+--- sourcebatch.sourcefiles is a list of source files, sourcebatch.objectfiles is a list of object files,
+--- and sourcebatch.rulename is the name of the corresponding rule.
+--- @endexample
+--- @see add_files
+---
+--- @return table<string, SourceBatch>
 function Target:sourcebatches() end
 
 ---
 --- Get a list of all object files
 ---
 --- [Open in browser](https://xmake.io/#/manual/target_instance?id=targetobjectfiles)
+---
+--- Although `target:sourcebatches()` can also obtain all object files, they are classified according to the source file type and do not directly participate in the final link.
+---
+--- If we want to dynamically modify the final linked object file list, we can modify `target:objectfiles()`, which is an array list.
 ---
 --- @return string[]
 function Target:objectfiles() end
@@ -151,11 +210,21 @@ function Target:objectfiles() end
 function Target:extrafiles() end
 
 ---
----Get a list of all header files
+--- Get a list of all header files
 ---
----[Open in browser](https://xmake.io/#/manual/target_instance?id=targetheaderfiles)
+--- [Open in browser](https://xmake.io/#/manual/target_instance?id=targetheaderfiles)
 ---
----@return string[]
+--- You can get a list of all header files set by the `add_headerfiles()` interface.
+---
+--- @example
+--- ```lua
+--- for _, headerfile in ipairs(target:headerfiles()) do
+---     - TODO
+--- end
+--- ```
+--- @endexample
+---
+--- @return string[]
 function Target:headerfiles() end
 
 ---
@@ -163,5 +232,47 @@ function Target:headerfiles() end
 ---
 ---[Open in browser](https://xmake.io/#/manual/target_instance?id=targetscriptdir)
 ---
----@return string
+--- This is usually used in custom rules. If you want to get the directory where the current target is actually defined in xmake.lua,
+--- it is convenient to reference some resource files. You can use this interface.
+---
+--- @return string
 function Target:scriptdir() end
+
+--- Check whether the target compilation configuration can obtain the given C++ function
+---
+--- The usage is similar to `target:has_cfuncs`, except that it is mainly used to detect C++ functions.
+--- However, while detecting functions, we can also additionally configure std languages to assist detection.
+---
+--- @example
+--- ```lua
+--- target:has_cxxfuncs("foo", {includes = "foo.h", configs = {languages = "cxx17"}})
+--- ```
+--- @endexample
+---
+--- @param name string function name
+--- @vararg table configs
+--- @return boolean # true if the target can obtain the given C/C++ function
+function Target:has_cxxfuncs(name, ...) end
+
+--- Check whether the target compilation configuration can obtain the given C type
+---
+--- @example
+--- This should be used in `on_config` like this:
+--- ```lua
+--- add_requires("zlib")
+--- target("test")
+---      set_kind("binary")
+---      add_files("src/*.c")
+---      add_packages("zlib")
+---      on_config(function(target)
+---          if target:has_ctypes("z_stream", {includes = "zlib.h"}) then
+---              target:add("defines", "HAVE_ZSTEAM_T")
+---          end
+---      end)
+--- ```
+--- @endexample
+---
+--- @param name string function name
+--- @vararg table configs
+--- @return boolean # true if the target can obtain the given C type
+function Target:has_ctypes(name, ...) end
